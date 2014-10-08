783da361528c0c38579d8a4cace953c3e8db511b
redaphid <iam@aaronherres.com>
2014-10-08 15:53:23 -0700

    mooooore stuufffff

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var configManager = require('./config-manager');
-require('./message-manager')(configManager);
+
+device = {name: 'test-subdevice', connector: 'meshblu-blink1', uuid: 'f105d101-4ea8-11e4-9133-338b9914afd1', token: '000xfoik6yptoi529egexh80t3rcc8fr'}
+
+var deviceManager = require('./device-manager')(configManager.loadConfig());
+deviceManager.setupDevice(device, deviceManager.startDevice);
