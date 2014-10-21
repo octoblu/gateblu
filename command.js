5548f84199eaa79793c17797d1947df5da800811
kmariano <koshin.mariano@gmail.com>
2014-10-21 16:14:46 -0700

    Adding the option to specify config path to load custom configuration.

--- command.js
+++ command.js
@@ -1,22 +1,38 @@
 var commander = require('commander');
-
+var fs = require('fs'); 
 commander
   .version(0.1)
   .option('-u, --uuid [uuid]',  'Meshblu UUID')
+  .option('-c, --config [configPath]', 'Path to config file')
   .option('-t, --token [token]',  'Meshblu Token')
   .option('--tmp-path [tmpPath]',  'Ephemeral path')
   .option('--device-path [devicePath]',  'Where are my devices')
+  .option('--server [server]', 'Meshblu Server')
+  .option('--port [port]', 'Meshblu Port' )
   .option('--node-path [nodePath]',  'Path for node')
   .parse(process.argv);
 
-if (!commander.uuid || !commander.token || !commander.tmpPath || !commander.devicePath || !commander.nodePath) {
-  commander.help()
-}
+  var configOptions = {}; 
+  
+  if(commander.config){
+	  console.log(commander.config);
+	  var commandFile = fs.readFileSync(commander.config)
+  	 
+  } else{
+	   if (!commander.uuid || !commander.token || !commander.tmpPath || !commander.devicePath || !commander.nodePath) {
+   			commander.help()
+		} 
+		else {
+			configOptions = {
+  			  uuid       : commander.uuid,
+  			  token      : commander.token,
+			  devicePath : commander.devicePath,
+			  nodePath   : commander.nodePath,
+			  tmpPath    : commander.tmpPath,
+			  server     : commander.server || 'meshblu.octoblu.com',
+			  port       : commander.port || '80'
+		  };
+		}
+  }
 
-var gatenu = require('./index')({
-  uuid       : commander.uuid,
-  token      : commander.token,
-  devicePath : commander.devicePath,
-  nodePath   : commander.nodePath,
-  tmpPath    : commander.tmpPath
-});
+var gatenu = require('./index')(configOptions);
